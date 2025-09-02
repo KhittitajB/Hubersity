6826fa44bea6700dc1b4724900ecf01dcdd2eb3e
Watcharapat PATHANUTPONG <watcharapat.p@ku.th>
2025-09-02 20:43:46 +0700

    Setup project structure, connect DB, and add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from . import models
+from .database import engine
+from .routers import users
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://your-frontend.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],        
+    allow_headers=["*"],     
+)
+
+
+try:
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    print("Database connection OK")
+except Exception as e:
+    print("Database connection failed:", e)
+
+app.include_router(users.router)
+
 @app.get("/")
 def root():
-    return {"post_details_list": "Hello World"}
+    return {"msg": "Welcome to Hubersity"}
---
+++ backend/app/routers/users.py
@@ -0,0 +1,30 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email is already in use"
+        )
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.get("/{id}", response_model=schemas.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User id:{id} was not found")
+    return user
